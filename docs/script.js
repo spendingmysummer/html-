d21e65cae926cecdbaa9996d83d94751339228d8
spendingmysummer <schlccntng@gmail.com>
2025-10-06 22:28:25 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,8 @@
-// 1. button 1
-const TombolSapa = document.getelementById('sapabutton');
-tombolSapa.addeventListener('click', function() {
-  alert('hi');l
-));
+<button id="sapabutton">Sapa Saya</button>
+
+<script>
+  const tombolSapa = document.getElementById('sapabutton');
+  tombolSapa.addEventListener('click', function() {
+    alert('Hi');
+  });
+</script>
